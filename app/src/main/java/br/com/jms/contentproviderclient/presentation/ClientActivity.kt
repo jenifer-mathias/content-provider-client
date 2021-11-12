164653c360d93f36304b21324242e54e9b007680
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2021-11-12 16:19:06 -0300

    Adds logic to hide button when scrolling
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

--- app/src/main/java/br/com/jms/contentproviderclient/presentation/ClientActivity.kt
+++ app/src/main/java/br/com/jms/contentproviderclient/presentation/ClientActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.jms.contentproviderclient.R
 import kotlinx.android.synthetic.main.activity_client.*
 
@@ -13,13 +14,14 @@ class ClientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client)
-
         getContentProvider()
 
         bt_refresh_client.setOnClickListener { getContentProvider() }
+
+        hideFloatingActionBtWhenScroll()
     }
 
-    private fun getContentProvider(){
+    private fun getContentProvider() {
         try {
             val url = "content://br.com.jms.contentprovider.provider/notes"
             val data = Uri.parse(url)
@@ -32,5 +34,16 @@ class ClientActivity : AppCompatActivity() {
         } catch (ex: Exception) {
             ex.printStackTrace()
         }
+    }
+
+    private fun hideFloatingActionBtWhenScroll() {
+        client_list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > 0 && bt_refresh_client.let { bt_refresh_client.isShown }) {
+                    bt_refresh_client.hide()
+                } else bt_refresh_client.show()
+            }
+        })
     }
 }
